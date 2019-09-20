046f2f42538747c3833b8f99e99c7b839ec83f42
cw655 <cw655@cornell.edu>
2019-09-20 15:24:14 -0400

    new code

--- sort-test.c
+++ sort-test.c
@@ -3,7 +3,7 @@
 
 void sort( int* x_ptr, int* y_ptr )
 {
-  if ( (*y_ptr) > (*x_ptr) ) {
+  if ( (*y_ptr) < (*x_ptr) ) {
     int temp = *x_ptr;
     *x_ptr   = *y_ptr;
     *y_ptr   = temp;
@@ -14,8 +14,12 @@ int main( void )
 {
   int a = 5;
   int b = 9;
+  int c = 3;
+  int d = 8;
   sort( &a, &b );
   UTST_ASSERT_INT_EQ( a, 5 );
   UTST_ASSERT_INT_EQ( b, 9 );
+  sort( &d, &c );
+  UTST_ASSERT_INT_EQ( d, 3 );
   return 0;
 }
